53de34b6a47208fbfa5eda6f5e95e4282524a3de
Eyal Ron <eyal670@gmail.com>
2019-10-15 12:21:47 +0300

    replace dashs with spaces

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,44 +1,62 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
   async prompting() {
-    this.log(
-      yosay(`Welcome to ${chalk.red('generator-elementor-widget')} generator!`)
-    );
-    this.answers = await this.prompt([{
-        type: 'input',
-        name: 'auth_name',
-        message: 'Your name:',
+    this.log(yosay(`Welcome to ${chalk.red("elementor-widget")} generator!`));
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "auth_name",
+        message: "Your name:"
       },
       {
-        type: 'input',
-        name: 'plugin_name',
-        message: 'Plugin name:',
+        type: "input",
+        name: "plugin_name",
+        message: "Plugin name:"
       },
       {
-        type: 'input',
-        name: 'widget_name',
-        message: 'Widget name:',
+        type: "input",
+        name: "widget_name",
+        message: "Widget name:"
       }
     ]);
   }
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath('pluginName.php'),
-      this.destinationPath(this.answers.plugin_name + '.php '), {
-        auth_name: this.answers.auth_name
-      } // user answer `title` used
+      this.templatePath("pluginName.php"),
+      this.destinationPath(this.answers.plugin_name + ".php "),
+      {
+        auth_name: this.answers.auth_name,
+        plugin_name: capitalizeFirstLetter(
+          spacing_name(this.answers.plugin_name)
+        )
+      }
     );
     this.fs.copyTpl(
-      this.templatePath('modules/widgetName/widgets/widget-widgetName.php'),
-      this.destinationPath('modules/' + this.answers.widget_name + '/widgets/widget-' + this.answers.widget_name + '.php '), {
+      this.templatePath("modules/widgetName/widgets/widget-widgetName.php"),
+      this.destinationPath(
+        "modules/" +
+          this.answers.widget_name +
+          "/widgets/widget-" +
+          this.answers.widget_name +
+          ".php "
+      ),
+      {
         auth_name: this.answers.auth_name,
         widget_name: this.answers.widget_name
-      } // user answer `title` used
+      }
     );
   }
 };
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function spacing_name(str) {
+  return str.replace(/\-/g, " ");
+}
